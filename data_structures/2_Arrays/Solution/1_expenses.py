44982b7189d9f17810a876bd3173b9224b481a47
Tarun S S <151313849+tarunsrepo@users.noreply.github.com>
2024-07-04 21:00:15 +0530

    Update 1_expenses.py

--- data_structures/2_Arrays/Solution/1_expenses.py
+++ data_structures/2_Arrays/Solution/1_expenses.py
@@ -1,37 +1,30 @@
-# 1. Let us say your expense for every month are listed below,
-# 	1. January -  2200
-#  	2. February - 2350
-#     3. March - 2600
-#     4. April - 2130
-#     5. May - 2190
-#
-# Create a list to store these monthly expenses and using that find out,
-#
+arr = [2200,2350,2600,2130,2190]
+
+# January - 2200
+# February - 2350
+# March - 2600
+# April - 2130
+# May - 2190
+
 # 1. In Feb, how many dollars you spent extra compare to January?
+print(arr[1]-arr[0])
+
 # 2. Find out your total expense in first quarter (first three months) of the year.
+print(arr[0]+arr[1]+arr[2])
+
 # 3. Find out if you spent exactly 2000 dollars in any month
+
+print(2000 in arr)
+
+
 # 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+
+arr.append(1980)
+print(arr)
+
 # 5. You returned an item that you bought in a month of April and
 # got a refund of 200$. Make a correction to your monthly expense list
 # based on this
 
-exp = [2200,2350,2600,2130,2190]
-
-# 1. In Feb, how many dollars you spent extra compare to January?
-print("In feb this much extra was spent compared to jan:",exp[1]-exp[0]) # 150
-
-# 2. Find out your total expense in first quarter (first three months) of the year
-print("Expense for first quarter:",exp[0]+exp[1]+exp[2]) # 7150
-
-# 3. Find out if you spent exactly 2000 dollars in any month
-print("Did I spent 2000$ in any month? ", 2000 in exp) # False
-
-# 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
-exp.append(1980)
-print("Expenses at the end of June:",exp) # [2200, 2350, 2600, 2130, 2190, 1980]
-
-# 5. You returned an item that you bought in a month of April and
-# got a refund of 200$. Make a correction to your monthly expense list
-# based on this
-exp[3] = exp[3] - 200
-print("Expenses after 200$ return in April:",exp) # [2200, 2350, 2600, 1930, 2190, 1980]
+arr[3] = arr[3] - 200
+print(arr)
